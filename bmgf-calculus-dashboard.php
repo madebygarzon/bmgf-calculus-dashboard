7637a3cafd2aa6132a65da395eb446b9c55474cf
Carlos Garzon <madebygarzon@gmail.com>
2026-02-25 11:01:10 -0500

    Update code

--- bmgf-calculus-dashboard.php
+++ bmgf-calculus-dashboard.php
@@ -3,7 +3,7 @@
  * Plugin Name: BMGF Calculus Market Dashboard
  * Plugin URI: https://partnerinpublishing.com
  * Description: Interactive dashboard for Math Education Market Analysis - Calculus textbook market data visualization.
- * Version: 32.0.0
+ * Version: 32.0.1
  * Author: Team Dev PIP
  * Author URI: https://partnerinpublishing.com
  * License: GPL v2 or later
